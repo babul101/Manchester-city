54186dc2a8a2bd0c7db2d88f9e798bce7e1c662c
babul101 <babul101@gmail.com>
2019-01-09 03:56:58 +0000

    Finishing the Blocks

--- src/Components/Home/Matches/Blocks.js
+++ src/Components/Home/Matches/Blocks.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { firebaseMatches } from '../../../firebase';
 import { firebaseLooper,reverseArray } from '../../ui/misc';
-
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 class Blocks extends Component {
 
@@ -18,10 +19,22 @@ class Blocks extends Component {
         })
     }
 
-    showMatches = () => (
-        <div>
-            match
-        </div>
+    showMatches = (matches) => (
+      matches ? matches.map((match)=>(
+          <Slide bottom key={match.id}>
+            <div className='item'>
+                <div className='wrapper'>
+                    <MatchesBlock match={match} />
+                </div>
+            </div>
+          </Slide>
+            
+      ))
+      
+      
+      
+      
+      :null  
 
     )
 
