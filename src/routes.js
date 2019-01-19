42c3bc223f2f6691bbd7cb52b61c6c65413e37c3
babul101 <babul101@gmail.com>
2019-01-19 09:10:30 +0000

    Editing the form

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,15 @@ import SignIn from './Components/signin';
 import Dashboard from './Components/Admin/Dashboard';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoute from './Components/authRoutes/publicRoutes';
-
+import AdminMatches from './Components/Admin/Matches';
+import AddEditMatch from './Components/Admin/Matches/addEditMatch';
 const Routes = (props) => {
   
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path='/admin_matches/edit_match/:id' exact component={AddEditMatch} />
+        <PrivateRoute {...props} path='/admin_matches' exact component={AdminMatches} />
         <PrivateRoute {...props} path='/dashboard' exact component={Dashboard} />
         <PublicRoute {...props} restricted={true} path = '/sign_in' exact component={SignIn} />
         <PublicRoute {...props} restricted={false} path='/' exact component={Home} />
